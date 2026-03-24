91e920e1c8d9c23beb263f5f009ec2410a4e3e86
Kalel-Fernandes <kalelfernandes.senai@gmail.com>
2026-03-24 08:11:50 -0300

    Código Revisado e Comentado

--- api.php
+++ api.php
@@ -1,19 +1,23 @@
 <?php
 
+// Implementação do arquivo base
 require_once "mensagem.php";
 
+// Transformando para GET
 $texto = $_GET['texto'] ?? null;
 $autor = $_GET['autor'] ?? null;
 $data  = $_GET['data'] ?? date("Y-m-d");
 
+// Verificação para exibição da mensagem
 if ($texto && $autor) {
-
+    // Mensagem Exibida
     $mensagem = new Mensagem($texto, $autor, $data);
-
+    // Chamando o arquivo JSON
     header('Content-Type: application/json');
     echo json_encode($mensagem);
 
 } else {
+    // Tratamento de exceção
     echo json_encode([
         "erro" => "Parâmetros texto e autor são obrigatórios"
     ]);
--- mensagem.php
+++ mensagem.php
@@ -1,22 +1,29 @@
 <?php
 
+// Criando a classe Mensagem
 class Mensagem implements JsonSerializable {
+    // Atributos classe Mensagem
     public string $texto;
     public string $autor;
     public string $data;
 
+    // Construtor que inicializa o objeto
     public function __construct($texto, $autor, $data) {
+        // This chama atributos da própria classe
         $this->texto = $texto;
         $this->autor = $autor;
         $this->data = $data;
     }
 
+    // Função de envio da Mensagem
     public function enviarMensagem() {
+        // Forma que aparece no Body Parameter
         return "Data: " . $this->data .
                " Autor: " . $this->autor .
                " Mensagem: " . $this->texto;
     }
 
+    // Transformando Mensagem em arquivo JSON
     public function jsonSerialize(): mixed {
         return [
             'texto' => $this->texto,
